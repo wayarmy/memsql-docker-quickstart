d7d77cb7f10dfdd1c0411cfe9c79d6508fe42414
Wayarmy <quanpc294@gmail.com>
2017-12-14 11:22:04 +0700

    Add new version 5.8.2

---
+++ memsql-5.8.2/setup-master.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+set -e
+set -x
+
+# Expects this file to export $OPS_VERSION and $MEMSQL_VERSION
+export OPS_VERSION=5.8.2
+export MEMSQL_VERSION=ae99ed27eb2ce5e9c07fba92f19bffe9823bdfd5
+
+VERSION_URL="http://versions.memsql.com/memsql-ops/$OPS_VERSION"
+MEMSQL_VOLUME_PATH="/memsql"
+OPS_URL=$(curl -s "$VERSION_URL" | jq -r .tar)
+
+# setup memsql user
+groupadd -r memsql --gid 1000
+useradd -r -g memsql -s /bin/false --uid 1000 \
+    -d /var/lib/memsql-ops -c "MemSQL Service Account" \
+    memsql
+
+# download ops
+curl -s $OPS_URL -o /tmp/memsql_ops.tar.gz
+
+# install ops
+mkdir /tmp/memsql-ops
+tar -xzf /tmp/memsql_ops.tar.gz -C /tmp/memsql-ops --strip-components 1
+/tmp/memsql-ops/install.sh \
+    --host 127.0.0.1 \
+    --no-cluster \
+    --ops-datadir /memsql-ops \
+    --memsql-installs-dir /memsql-ops/installs
+
+DEPLOY_EXTRA_FLAGS=
+if [[ $MEMSQL_VERSION != "developer" ]]; then
+    DEPLOY_EXTRA_FLAGS="--version-hash $MEMSQL_VERSION"
+fi
+
+memsql-ops memsql-deploy --role master --license 35e6e509d157463fbf9d5b60d58e3301 --version-hash ae99ed27eb2ce5e9c07fba92f19bffe9823bdfd5
+
+MASTER_ID=$(memsql-ops memsql-list --memsql-role=master -q)
+MASTER_PATH=$(memsql-ops memsql-path $MASTER_ID)
+
+
+function setup_node_dirs {
+    local node_name=$1
+    local node_id=$2
+    local node_path=$3
+
+    # update socket file
+    memsql-ops memsql-update-config --key "socket" --value $node_path/memsql.sock $node_id
+
+    mkdir -p /memsql/$node_name
+
+    for tgt in data plancache tracelogs; do
+        # update the volume template
+        cp -r $node_path/$tgt /memsql/$node_name
+
+        # symlink the dir
+        rm -rf $node_path/$tgt
+        ln -s $MEMSQL_VOLUME_PATH/$node_name/$tgt $node_path/$tgt
+    done
+
+    # clear the plancache
+    rm -rf /memsql/$node_name/plancache/*
+}
+
+setup_node_dirs master $MASTER_ID $MASTER_PATH
+
+chown -R memsql:memsql /memsql /memsql-ops
+
+memsql-ops memsql-stop --all
+memsql-ops stop
+
+# cleanup
+rm -rf /tmp/*
+rm -rf /memsql-ops/data/cache/*
